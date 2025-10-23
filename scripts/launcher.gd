8c359037926df182710eaa4b7f1dc5b97e30db30
Kirill Kurtov <122525465+GeeksterVALO@users.noreply.github.com>
2025-10-23 15:58:14 +0300

    logs

--- scripts/launcher.gd
+++ scripts/launcher.gd
@@ -1,14 +1,21 @@
 extends Control
 
+
 @onready var settings_panel: Panel = $MainContainer/ContentContainer/SettingsPanel
 @onready var news_section: Panel = $MainContainer/ContentContainer/NewsSection
 @onready var sidebar: Panel = $MainContainer/Sidebar
 @onready var header: Panel = $Header
 @onready var footer: Panel = $Footer
+@onready var webview = $WebView
+
 
 func _ready() -> void:
 	# Здесь можно инициализировать UI, загрузить новости, применить настройки и т.д.
-	pass
+	# Подключаем обработчик логов из JS через godot_wry bridge
+	webview.bind("godot_log", _on_js_log)
+
+func _on_js_log(msg):
+	print("[JS]", msg)
 
 func toggle_settings_panel() -> void:
 	settings_panel.visible = not settings_panel.visible
