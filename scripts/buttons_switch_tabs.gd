0498f911d4d9f61f50e8a37dd678b1eb75d1c323
AleD93 <adworld.aled@gmail.com>
2025-10-27 03:07:09 +0900

    Implement geekster's ui concept

---
+++ scripts/buttons_switch_tabs.gd
@@ -0,0 +1,27 @@
+extends TabContainer
+
+@export var buttons_group: ButtonGroup
+
+
+func _ready() -> void:
+	buttons_group.pressed.connect(_on_group_button_pressed)
+	tab_changed.connect(_on_tab_changed)
+
+	_update_pressed_button()
+
+
+func _on_group_button_pressed(button: BaseButton) -> void:
+	var new_tab_index: int = button.get_meta("tab_index", -1)
+	if new_tab_index >= 0:
+		current_tab = new_tab_index
+
+
+func _on_tab_changed(tab_index: int) -> void:
+	_update_pressed_button()
+
+
+func _update_pressed_button() -> void:
+	for btn: BaseButton in buttons_group.get_buttons():
+		if btn.get_meta("tab_index", -1) == current_tab:
+			btn.button_pressed = true
+			break
