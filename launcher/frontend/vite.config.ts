e510899f887d29f987bb825ef5654c4c2ee856d2
milanjrodd <milanrodd@gmail.com>
2025-11-07 16:20:40 +0300

    fix memory leak in wails dev mode

--- launcher/frontend/vite.config.ts
+++ launcher/frontend/vite.config.ts
@@ -7,14 +7,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), tailwindcss(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    watch: {
+      // Prevent vite from leaking memory in Windows, when running with wails
+      // https://github.com/wailsapp/wails/issues/3903
+      ignored: ['**/wailsjs/**'],
     },
   },
 })
