d3aa43971d680bb31a5d6f9fea7bab5b8ec67273
milanjrodd <milanrodd@gmail.com>
2025-11-26 13:04:24 +0300

    fix auth

--- launcher-rust/src-tauri/src/lib.rs
+++ launcher-rust/src-tauri/src/lib.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::{BufWriter, Read};
 use std::path::Path;
 use tar::{Archive, Builder};
+use tauri::LogicalSize;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -231,30 +232,32 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_upload::init())
         .plugin(tauri_plugin_os::init())
-        .setup(|_app| {
-            // let app_ = app.handle().clone();
-            // let window =
-            //     WebviewWindowBuilder::new(app, "main", WebviewUrl::App("index.html".into()))
-            //         .on_new_window(move |url, features| {
-            //             // Create a new window with a unique label
-            //             let builder = tauri::WebviewWindowBuilder::new(
-            //                 &app_,
-            //                 "opened-window", // Ideally use a counter for multiple windows
-            //                 tauri::WebviewUrl::External(url.clone()),
-            //             )
-            //             .window_features(features)
-            //             .on_document_title_changed(|window, title| {
-            //                 window.set_title(&title).unwrap();
-            //             })
-            //             .title(url.as_str());
+        .setup(|app| {
+            let app_ = app.handle().clone();
+            let window = tauri::WebviewWindowBuilder::from_config(
+                app.handle(),
+                &app.config().app.windows[0],
+            )?
+            .on_new_window(move |url, features| {
+                // Create a new window with a unique label
+                let builder = tauri::WebviewWindowBuilder::new(
+                    &app_,
+                    "opened-window", // Ideally use a counter for multiple windows
+                    tauri::WebviewUrl::External(url.clone()),
+                )
+                .window_features(features)
+                .on_document_title_changed(|window, title| {
+                    window.set_title(&title).unwrap();
+                })
+                .title(url.as_str());
 
-            //             match builder.build() {
-            //                 Ok(window) => tauri::webview::NewWindowResponse::Create { window },
-            //                 Err(_) => tauri::webview::NewWindowResponse::Deny,
-            //             }
-            //         })
-            //         .build()?;
-            // window.set_min_size(Some(LogicalSize::new(800, 600)))?;
+                match builder.build() {
+                    Ok(window) => tauri::webview::NewWindowResponse::Create { window },
+                    Err(_) => tauri::webview::NewWindowResponse::Deny,
+                }
+            })
+            .build()?;
+            window.set_min_size(Some(LogicalSize::new(800, 600)))?;
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
--- launcher-rust/src/pages/AuthPage.vue
+++ launcher-rust/src/pages/AuthPage.vue
@@ -4,20 +4,16 @@ import { cn } from '@/lib/utils'
 import { useAuth } from '@/lib/usePocketbase'
 import { useRouter } from 'vue-router'
 import type { AuthMethodsList } from 'pocketbase'
+// import Input from '@/components/ui/Input.vue'
 
 const auth = useAuth()
 const router = useRouter()
 
-const email = ref('')
-const password = ref('')
+// TODO: Implement OTP with email
+// const email = ref('')
 const authMethods = ref<AuthMethodsList>()
 
 auth.listAuthMethods().then((methods) => (authMethods.value = methods))
-
-const onFormSubmit = async () => {
-  await auth.authUserWithPassword({ email: email.value, password: password.value })
-  router.push('/')
-}
 
 const onLoginWithOAuth = async (provider: string) => {
   await auth.authWithOAuth2(provider)
@@ -29,64 +25,23 @@ const onLoginWithOAuth = async (provider: string) => {
   <div class="flex h-screen w-full items-center justify-center px-4">
     <div :class="cn('flex min-w-80 flex-col gap-6')">
       <div class="min-h-80">
-        <div>
-          <div>Login to your account</div>
-          <div>Enter your email below to login to your account</div>
-        </div>
-        <div>
-          <form @submit.prevent="onFormSubmit">
-            <div class="flex flex-col gap-6">
-              <div class="grid gap-3">
-                <Label for="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                  v-model="email"
-                  :disabled="auth.loading.value"
-                />
-              </div>
-              <div class="grid gap-3">
-                <div class="flex items-center">
-                  <Label for="password">Password</Label>
-                  <a
-                    href="#"
-                    class="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <input
-                  id="password"
-                  type="password"
-                  required
-                  v-model="password"
-                  :disabled="auth.loading.value"
-                />
-              </div>
-              <div class="flex flex-col gap-3">
-                <button type="submit" class="w-full" :disabled="auth.loading.value">Login</button>
-                <template v-if="authMethods && authMethods.oauth2.enabled">
-                  <button
-                    type="button"
-                    variant="outline"
-                    class="w-full"
-                    v-for="provider in authMethods.oauth2.providers"
-                    :key="provider.name"
-                    :disabled="auth.loading.value"
-                    @click="onLoginWithOAuth(provider.name)"
-                  >
-                    Login with {{ provider.displayName }}
-                  </button>
-                </template>
-              </div>
-            </div>
-            <div class="mt-4 text-center text-sm">
-              Don't have an account?
-              <a href="#" class="underline underline-offset-4"> Sign up </a>
-            </div>
-          </form>
+        <h1 class="w-full text-center text-3xl">Zapuskalka</h1>
+        <div class="flex flex-col gap-6">
+          <div class="flex flex-col gap-3">
+            <template v-if="authMethods && authMethods.oauth2.enabled">
+              <button
+                type="button"
+                variant="outline"
+                class="w-full"
+                v-for="provider in authMethods.oauth2.providers"
+                :key="provider.name"
+                :disabled="auth.loading.value"
+                @click="onLoginWithOAuth(provider.name)"
+              >
+                Login with {{ provider.displayName }}
+              </button>
+            </template>
+          </div>
         </div>
       </div>
     </div>
