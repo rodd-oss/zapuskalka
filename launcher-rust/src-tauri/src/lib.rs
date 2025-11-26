e81c6466dd6c577b47926bb7de50eb9443790c6b
milanjrodd <milanrodd@gmail.com>
2025-11-26 11:49:14 +0300

    init launcher updater

--- launcher-rust/src-tauri/src/lib.rs
+++ launcher-rust/src-tauri/src/lib.rs
@@ -224,6 +224,7 @@ async fn upload_file_as_form_data(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
--- launcher-rust/src/App.vue
+++ launcher-rust/src/App.vue
@@ -1,5 +1,40 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+
+import { check } from '@tauri-apps/plugin-updater'
+import { relaunch } from '@tauri-apps/plugin-process'
+import { onMounted } from 'vue'
+
+onMounted(async () => {
+  const update = await check()
+  if (update) {
+    console.log(`found update ${update.version} from ${update.date} with notes ${update.body}`)
+    let downloaded = 0
+    let contentLength = 0
+    // alternatively we could also call update.download() and update.install() separately
+    await update.downloadAndInstall((event) => {
+      switch (event.event) {
+        case 'Started':
+          if (event.data.contentLength == undefined) {
+            throw new Error('Content length undefined')
+          }
+          contentLength = event.data.contentLength
+          console.log(`started downloading ${event.data.contentLength} bytes`)
+          break
+        case 'Progress':
+          downloaded += event.data.chunkLength
+          console.log(`downloaded ${downloaded} from ${contentLength}`)
+          break
+        case 'Finished':
+          console.log('download finished')
+          break
+      }
+    })
+
+    console.log('update installed')
+    await relaunch()
+  }
+})
 </script>
 
 <template><RouterView /></template>
