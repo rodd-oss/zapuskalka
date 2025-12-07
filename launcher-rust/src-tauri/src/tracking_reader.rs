33082198b4f40867160fd7c8c9564d531c3afade
AleD93 <adworld.aled@gmail.com>
2025-12-07 17:07:31 +0900

    Fix clippy's "question_mark"

--- launcher-rust/src-tauri/src/tracking_reader.rs
+++ launcher-rust/src-tauri/src/tracking_reader.rs
@@ -13,12 +13,9 @@ impl<T: Read, F: FnMut(&mut [u8])> TrackingReader<T, F> {
 
 impl<T: Read, F: FnMut(&mut [u8])> Read for TrackingReader<T, F> {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        let res = self.source.read(buf);
-        if res.is_err() {
-            return res;
-        }
+        let n = self.source.read(buf)?;
 
         (self.callback)(buf);
-        res
+        Ok(n)
     }
 }
